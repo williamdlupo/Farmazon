aff17b7cea221aca7dc7c64c96e852efb803339c
Joshua McKelvey <globle.jn@hotmail.com>
2019-02-03 00:51:59 -0600

    Beginning or Market and Farmer Profile

--- Farmazon/Models/IdentityModels.cs
+++ Farmazon/Models/IdentityModels.cs
@@ -9,7 +9,6 @@ namespace Farmazon.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string Hometown { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
