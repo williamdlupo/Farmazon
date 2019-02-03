1402388a98d97e48d0dd188f7bab8671cc2a405a
Joshua McKelvey <globle.jn@hotmail.com>
2019-02-03 11:16:08 -0600

    Added Texting Functionality

--- Farmazon/Controllers/ProductController.cs
+++ Farmazon/Controllers/ProductController.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
-
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 namespace Farmazon.Controllers
 {
     [Authorize]
@@ -12,5 +23,26 @@ namespace Farmazon.Controllers
         {
             return View();
         }
+
+        //
+        // POST: /Account/Createfarm
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> CreateFarm(Farm farmModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        using (var context = new Farmazon_dbEntities())
+        //        {
+        //            var userId = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+
+        //            context.CreateInventoryItem(null, userId, null, 5);
+        //            await context.SaveChangesAsync();
+
+        //            return RedirectToAction("Createfarm", "Account");
+        //        }
+        //    }
+        //    return View(farmModel);
+        //}
     }
 }
